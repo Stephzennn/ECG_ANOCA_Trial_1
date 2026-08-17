f2782a1080bb3fb317c57efe8459892fdef571f7
Stephzennn <estifanosSNzen@outlook.com>
2026-08-17 08:24:57 -0700

    Clean up the first half of the embedding.py file

--- Models/ECGFounder-master/ECGFounder-master/embedding.py
+++ Models/ECGFounder-master/ECGFounder-master/embedding.py
@@ -26,24 +26,12 @@ import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
 import torchvision.transforms as transforms
 from scipy.interpolate import interp1d
-
-
 #from sklearn.decomposition import PCA , TSNE
-
 from sklearn.manifold import TSNE
 device = torch.device('cuda:{}'.format(0) if torch.cuda.is_available() else 'cpu')
 
 print(device)
 
-
-# Create a dummy input tensor with shape (batch_size, channels, length)
-x = torch.randn(64, 12, 5000)
-
-# Move the input tensor to the same device as the model
-x = x.to(device)
-
-
-# Try with real dataset.
 
 saved_dir = './res/eval'
 csv_filepath = './csv/ptbxl_label.csv'
@@ -53,16 +41,21 @@ ecg_filepath = './data/ptb-xl-a-large-publicly-available-electrocardiography-dat
 all_embeddings = []
 all_labels = []
 
-from ptbxlModule import PTBXL_Dataset
-tasks = []
-batch_size = 512
-with open(os.path.join('./tasks.txt'), 'r') as fin: 
-    for line in fin:
-        tasks.append(line.strip())
+def createDataloader(task_Path, batch_Size, ecg_filepath, csv_path, num_workers = 0 ):
+    from ptbxlModule import PTBXL_Dataset
+    tasks = []
+    batch_size = batch_Size
+    #'./tasks.txt'
+    with open(os.path.join(task_Path), 'r') as fin: 
+        for line in fin:
+            tasks.append(line.strip())
+    #csv_filepath
+    testset = PTBXL_Dataset(ecg_path=ecg_filepath, csv_path=csv_path)
+    testloader = DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers= num_workers) #(os.cpu_count())
 
-testset = PTBXL_Dataset(ecg_path=ecg_filepath, csv_path=csv_filepath)
-testloader = DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers= 0) #(os.cpu_count())
+    return testset, testloader
 
+testset, testloader = createDataloader('./tasks.txt', 512, ecg_filepath,csv_filepath)
 
 
 
@@ -85,6 +78,23 @@ model = Net1D(
 
 model.to(device)
 
+def loadWeightsToModel(weightPath,model,device):
+    checkpoint = torch.load(weightPath, map_location=device)
+    state_dict = checkpoint['state_dict']
+
+    log = model.load_state_dict(state_dict, strict=False)
+
+    for name, param in model.named_parameters():
+        param.requires_grad = False
+
+    model.to(device)
+
+    model.eval()
+    return log
+    
+log = loadWeightsToModel('./checkpoint/12_lead_ECGFounder.pth', model, device)
+
+"""
 checkpoint = torch.load('./checkpoint/12_lead_ECGFounder.pth', map_location=device)
 state_dict = checkpoint['state_dict']
 
@@ -97,17 +107,12 @@ model.to(device)
 
 model.eval()
 
-
-#_, deep_features = model(x)
-
-
-
-#print(deep_features.shape)  # Should print the shape of the output features
+"""
 
 
 # Here try to exract the deep features for every sample and the labels
 
-
+"""
 prog_iter_test = tqdm(testloader, desc="Testing", leave=False)
 all_gt = []
 all_pred_prob = []
@@ -128,30 +133,66 @@ all_embeddings = np.concatenate(all_embeddings)
 labels = np.concatenate(all_gt)
 df_gt = pd.DataFrame(all_gt)
 
-all_gt.shape
+"""
 
-afib_gt = all_gt[:, 2]
+def generateOutput(testloader, model, device):
+    prog_iter_test = tqdm(testloader, desc="Testing", leave=False)
+    all_gt = []
+    all_pred_prob = []
+    all_thre_df = []
+    all_embeddings = []
 
-afib_gt = afib_gt.reshape(afib_gt.shape[0], 1)
-afib_pred_prob = all_pred_prob[:, 2]
+    with torch.no_grad():
+        for batch_idx, batch in enumerate(prog_iter_test):
+            input_x, input_y = tuple(t.to(device) for t in batch)
+            logits, deep_features = model(input_x)
+            pred = F.sigmoid(logits)
+            all_pred_prob.append(pred.cpu().data.numpy())
+            all_gt.append(input_y.cpu().data.numpy())
+            all_embeddings.append(deep_features.cpu().data.numpy())
+    all_pred_prob = np.concatenate(all_pred_prob)
+    all_gt = np.concatenate(all_gt)
+    all_embeddings = np.concatenate(all_embeddings)
+    labels = np.concatenate(all_gt)
+    df_gt = pd.DataFrame(all_gt)
+    return all_gt, all_embeddings, df_gt, labels, all_pred_prob
 
-afib_pred_prob = afib_pred_prob.reshape(afib_pred_prob.shape[0], 1)
-
-afib_gt.shape, afib_pred_prob.shape
-res_test, res_test_auroc, res_test_sens, res_test_spec, res_test_f1, optimal_thresholds = eval_with_dynamic_thresh(afib_gt, afib_pred_prob)
+all_gt, all_embeddings, df_gt, labels, all_pred_prob = generateOutput(testloader, model, device)
 
 
-label_two = []
-for x in range(df_gt.shape[0]):
-    if df_gt.iloc[x][2] == 1:
-        label_two.append(1)
-    else:
-        label_two.append(0)
+def extractResults(task: int, all_gt, all_pred_prob, taskPath):
+    dd = "Place Holder"
+    counter = 0
+    with open(os.path.join(taskPath), 'r') as fin: 
+            for line in fin:
+                if counter == task:
+                    dd = line.strip()
+                    break
+                counter += 1
 
-type(label_two), len(label_two)
-all_gt
-type(all_embeddings), all_embeddings.shape, type(labels), labels.shape
-label_two = np.array(label_two)
+    print(f"Extract Results for {dd}")
+    afib_gt = all_gt[:, task]
+    afib_gt = afib_gt.reshape(afib_gt.shape[0], 1)
+    afib_pred_prob = all_pred_prob[:, task]
+
+    afib_pred_prob = afib_pred_prob.reshape(afib_pred_prob.shape[0], 1)
+
+    afib_gt.shape, afib_pred_prob.shape
+    res_test, res_test_auroc, res_test_sens, res_test_spec, res_test_f1, optimal_thresholds = eval_with_dynamic_thresh(afib_gt, afib_pred_prob)
+
+    label_two = []
+    for x in range(df_gt.shape[0]):
+        if df_gt.iloc[x][task] == 1:
+            label_two.append(1)
+        else:
+            label_two.append(0)
+    label_two = np.array(label_two)
+
+    return res_test, res_test_auroc, res_test_sens, res_test_spec, res_test_f1, optimal_thresholds, label_two, afib_gt, afib_pred_prob, dd
+    
+
+res_test, res_test_auroc, res_test_sens, res_test_spec, res_test_f1, optimal_thresholds, label_two, afib_gt, afib_pred_prob, TaskName = extractResults(5, all_gt, all_pred_prob,'./tasks.txt')
+
 # For every output, extract PCA components of 50. and save it in a new file.
 
 
@@ -163,9 +204,8 @@ label_two = np.array(label_two)
 # For the extracted per sample TSNE (2), extract 2 dimensional UMAP and save it in a new file.
 
 def run_tsne(embeddings, n_samples):
-    #perplexity = args.tsne_perplexity or min(30, max(5, n_samples // 10))
     perplexity =  min(30, max(5, n_samples // 10))
-        
+    
     print(f"  t-SNE: perplexity={perplexity}, n_iter={1000}")
     tsne = TSNE(
         n_components=2,
@@ -175,9 +215,6 @@ def run_tsne(embeddings, n_samples):
         verbose=0,
     )
     return tsne.fit_transform(embeddings)
-
-
-# For the extracted per sample UMAP (2), plott the in a cluster and color it based on the label of the sample.
 
 
 tsne_results = run_tsne(all_embeddings, all_embeddings.shape[0])
@@ -191,7 +228,7 @@ print(tsne_results.shape)
 
 
 
-def save_individual_plot(z, labels, out_path):
+def save_individual_plot(z, labels, out_path, taskName):
     n_total = len(labels)
     n_pos   = int((labels == 1).sum())
     n_neg   = int((labels == 0).sum())
@@ -199,10 +236,10 @@ def save_individual_plot(z, labels, out_path):
     fig, ax = plt.subplots(figsize=(7, 6))
     ax.scatter(z[labels == 0, 0], z[labels == 0, 1],
                c="#4878d0", alpha=0.55, s=18, linewidths=0,
-               label=f"SUPRAVENTRICULAR TACHYCARDIA-Negative  (n={n_neg})")
+               label=f"{taskName}-Negative  (n={n_neg})")
     ax.scatter(z[labels == 1, 0], z[labels == 1, 1],
                c="#ed3bde", alpha=0.75, s=28, linewidths=0,
-               label=f"SUPRAVENTRICULAR TACHYCARDIA-Positive  (n={n_pos})")
+               label=f"{taskName}-Positive  (n={n_pos})")
 
     mode_tag = "pretrained backbone"
     ax.set_title(
@@ -220,13 +257,9 @@ def save_individual_plot(z, labels, out_path):
     plt.close(fig)
     print(f"  Saved → {out_path}")
 
+out_path = os.path.join(r"C:\Users\Estif\Downloads\Langone\ANOCA\ECG_ANOCA_Trial_1\Models\ECGFounder-master\ECGFounder-master\Images", f"Trialembedding_tsne.png")
 
-
-
-
-out_path = os.path.join(r"C:\Users\Estif\Downloads\Langone\ANOCA\ECG_ANOCA_Trial_1\Models\ECGFounder-master\ECGFounder-master\Images", f"embedding_tsne.png")
-
-save_individual_plot(tsne_results, label_two, out_path)
+save_individual_plot(tsne_results, label_two, out_path, TaskName)
 
 
 
